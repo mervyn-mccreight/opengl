e601f726d2f9ddc3e2b5824d4069d49127176b53
Mervyn McCreight <mervyn.mccreight@gmail.com>
2014-11-17 10:33:24 +0100

    lesson 4 completed.

--- src/main/java/de/fhwedel/opengl/RenderLoop.java
+++ src/main/java/de/fhwedel/opengl/RenderLoop.java
@@ -15,16 +15,87 @@ import java.nio.IntBuffer;
 
 public class RenderLoop implements GLEventListener {
 
-    private long lastTime = System.currentTimeMillis();
-
     // An array of 3 vectors which represents 3 vertices
     float vertexBufferData[] = {
-            -1.0f, -1.0f, 0.0f,
-            1.0f, -1.0f, 0.0f,
-            0.0f, 1.0f, 0.0f,
+            -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+            -1.0f,-1.0f, 1.0f,
+            -1.0f, 1.0f, 1.0f, // triangle 1 : end
+            1.0f, 1.0f,-1.0f, // triangle 2 : begin
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f,-1.0f, // triangle 2 : end
+            1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f, 1.0f,
+            -1.0f,-1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f,-1.0f,
+            -1.0f, 1.0f,-1.0f,
+            1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f,-1.0f,
+            -1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f
     };
+    float colorBufferData[] = {
+            0.583f,  0.771f,  0.014f,
+            0.609f,  0.115f,  0.436f,
+            0.327f,  0.483f,  0.844f,
+            0.822f,  0.569f,  0.201f,
+            0.435f,  0.602f,  0.223f,
+            0.310f,  0.747f,  0.185f,
+            0.597f,  0.770f,  0.761f,
+            0.559f,  0.436f,  0.730f,
+            0.359f,  0.583f,  0.152f,
+            0.483f,  0.596f,  0.789f,
+            0.559f,  0.861f,  0.639f,
+            0.195f,  0.548f,  0.859f,
+            0.014f,  0.184f,  0.576f,
+            0.771f,  0.328f,  0.970f,
+            0.406f,  0.615f,  0.116f,
+            0.676f,  0.977f,  0.133f,
+            0.971f,  0.572f,  0.833f,
+            0.140f,  0.616f,  0.489f,
+            0.997f,  0.513f,  0.064f,
+            0.945f,  0.719f,  0.592f,
+            0.543f,  0.021f,  0.978f,
+            0.279f,  0.317f,  0.505f,
+            0.167f,  0.620f,  0.077f,
+            0.347f,  0.857f,  0.137f,
+            0.055f,  0.953f,  0.042f,
+            0.714f,  0.505f,  0.345f,
+            0.783f,  0.290f,  0.734f,
+            0.722f,  0.645f,  0.174f,
+            0.302f,  0.455f,  0.848f,
+            0.225f,  0.587f,  0.040f,
+            0.517f,  0.713f,  0.338f,
+            0.053f,  0.959f,  0.120f,
+            0.393f,  0.621f,  0.362f,
+            0.673f,  0.211f,  0.457f,
+            0.820f,  0.883f,  0.371f,
+            0.982f,  0.099f,  0.879f
+    };
+    private long lastTime = System.currentTimeMillis();
     private int vertexBufferId;
     private int programId;
+    private int colorBufferId;
 
 
     @Override
@@ -41,7 +112,7 @@ public class RenderLoop implements GLEventListener {
         gl.glEnable(GL.GL_CULL_FACE);
 
         gl.glEnable(GL.GL_DEPTH_TEST);
-        gl.glDepthFunc(GL.GL_LEQUAL);
+        gl.glDepthFunc(GL.GL_LESS);
 
 
         GL3 gl3 = gl.getGL3();
@@ -56,6 +127,12 @@ public class RenderLoop implements GLEventListener {
         gl3.glBindBuffer(GL3.GL_ARRAY_BUFFER, vertexBufferId);
         gl3.glBufferData(GL3.GL_ARRAY_BUFFER, vertexBufferData.length * Buffers.SIZEOF_FLOAT, FloatBuffer.wrap(vertexBufferData), GL3.GL_STATIC_DRAW);
 
+        IntBuffer colorBuffer = IntBuffer.allocate(1);
+        gl3.glGenBuffers(1, colorBuffer);
+        colorBufferId = colorBuffer.get(0);
+        gl3.glBindBuffer(GL3.GL_ARRAY_BUFFER, colorBufferId);
+        gl3.glBufferData(GL3.GL_ARRAY_BUFFER, colorBufferData.length * Buffers.SIZEOF_FLOAT, FloatBuffer.wrap(colorBufferData), GL3.GL_STATIC_DRAW);
+
         programId = loadShaders(gl3);
     }
 
@@ -66,11 +143,14 @@ public class RenderLoop implements GLEventListener {
         System.out.println("Compiling vertex shader");
         String vertexShaderSource[] = {"#version 330 core\n" +
                 "in vec3 vertexPosition_modelspace;\n" +
+                "layout(location = 1) in vec3 vertexColor;\n" +
+                "out vec3 fragmentColor;\n" +
                 "uniform mat4 MVP;\n" +
 
                 "void main(){\n" +
                 "   vec4 v = vec4(vertexPosition_modelspace,1);\n" +
                 "   gl_Position = MVP * v;\n" +
+                "   fragmentColor = vertexColor;\n" +
                 "}"};
 
         gl3.glShaderSource(vertexShaderId, 1, vertexShaderSource, null);
@@ -90,9 +170,10 @@ public class RenderLoop implements GLEventListener {
         // Compile Fragment Shader
         System.out.println("Compiling fragment shader");
         String fragmentShaderSource[] = {"#version 330 core\n" +
+                "in vec3 fragmentColor;\n" +
                 "out vec3 color;\n" +
                 "void main(){\n" +
-                "    color = vec3(1,0,0);\n" +
+                "    color = fragmentColor;\n" +
                 "}"};
         gl3.glShaderSource(fragmentShaderId, 1, fragmentShaderSource, null);
         gl3.glCompileShader(fragmentShaderId);
@@ -155,14 +236,23 @@ public class RenderLoop implements GLEventListener {
         //render objects here.
         gl3.glEnableVertexAttribArray(0);
         gl3.glBindBuffer(GL3.GL_ARRAY_BUFFER, vertexBufferId);
-        gl3.glVertexAttribPointer(0,    // no reason
+        gl3.glVertexAttribPointer(0,    // index of attribute (vertex, color...)
                 3,    // number of vertices
                 GL3.GL_FLOAT,  // type
                 false, // normalized?
                 0,    // stride (Schrittweite)
+                0);   // offset
+
+        gl3.glEnableVertexAttribArray(1);
+        gl3.glBindBuffer(GL3.GL_ARRAY_BUFFER, colorBufferId);
+        gl3.glVertexAttribPointer(1,
+                3,
+                GL3.GL_FLOAT,
+                false,
+                0,
                 0);
 
-        gl3.glDrawArrays(GL3.GL_TRIANGLES, 0, 3); // starting from 0, 3 vertices total
+        gl3.glDrawArrays(GL3.GL_TRIANGLES, 0, 12*3); // starting from 0, 12*3 vertices total
         gl3.glDisableVertexAttribArray(0);
     }
 
@@ -175,8 +265,8 @@ public class RenderLoop implements GLEventListener {
                             0,0,0, // look-at
                             0,1,0); // up.
         pmvMatrix.update();
-        System.out.println("ModelView: ");
-        System.out.println(PMVMatrix.matrixToString(new StringBuilder(), "%10.5f", pmvMatrix.glGetMatrixf()).toString());
+//        System.out.println("ModelView: ");
+//        System.out.println(PMVMatrix.matrixToString(new StringBuilder(), "%10.5f", pmvMatrix.glGetMatrixf()).toString());
 
         pmvMatrix.glMatrixMode(GLMatrixFunc.GL_PROJECTION);
         pmvMatrix.glLoadIdentity();
@@ -185,8 +275,8 @@ public class RenderLoop implements GLEventListener {
                                 0.1f,                                           // z-near
                                 100f);                                          // z-far
         pmvMatrix.update();
-        System.out.println("Projection: ");
-        System.out.println(PMVMatrix.matrixToString(new StringBuilder(), "%10.5f", pmvMatrix.glGetMatrixf(GLMatrixFunc.GL_PROJECTION)).toString());
+//        System.out.println("Projection: ");
+//        System.out.println(PMVMatrix.matrixToString(new StringBuilder(), "%10.5f", pmvMatrix.glGetMatrixf(GLMatrixFunc.GL_PROJECTION)).toString());
 
 //        This also works but "pollutes" the projection matrix with modelview data
 //        pmvMatrix.glMultMatrixf(pmvMatrix.glGetMatrixf(GLMatrixFunc.GL_MODELVIEW));
